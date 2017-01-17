5d0864118bd2d39a17ead1af174eeff117a4689b
tange3421 <b15.l.m.n.o@gmail.com>
2017-01-18 02:39:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+/***ここから追加***/
+// POSTパラメータ取得用 body-parser設定 (express4から必要)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Date()で現在時刻を取得するためのユーティリティ
+var dateutil = require('date-utils');
+ 
+app.post('/', function(req, res){
+ var date = new Date();
+ var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+ req.body.date = now;
+ 
+ console.log('app.js req.body: ' + JSON.stringify(req.body));
+ res.send(req.body);
+});
+/***ここまで追加***/
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
